d9d8d7f876526577fbb1425c0c6223bc69f610a6
anacamello <ana.camello@gmail.com>
2025-02-26 19:51:17 -0300

    Update Analise_DO.py

--- Analise_DO.py
+++ Analise_DO.py
@@ -22,7 +22,7 @@ if not os.path.exists(nltk_data_dir):
     os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.clear()
 nltk.data.path.append(nltk_data_dir)
-#nltk.download("stopwords", download_dir=nltk_data_dir)
+nltk.download("stopwords", download_dir=nltk_data_dir)
 nltk.download('punkt', download_dir=nltk_data_dir)
 nltk.download('punkt_tab', download_dir=nltk_data_dir)
 
